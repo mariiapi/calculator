c989e3de1ef39e645d4cf7f02313bfdd7830b768
mariiapi <mashapisnya@gmail.com>
2024-10-10 20:45:52 +0300

    done visuals and onCreate setting

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvResult: TextView
+
+    private lateinit var etLeft:  EditText
+    private lateinit var etRight: EditText
+
+    private lateinit var btnAdd:      Button
+    private lateinit var btnSubtract: Button
+    private lateinit var btnMultiply: Button
+    private lateinit var btnDivide:   Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etLeft = findViewById(R.id.etLeft)
+        etRight = findViewById(R.id.etRight)
+
+        tvResult = findViewById(R.id.tvResult)
+
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSubtract = findViewById(R.id.btnSubtract)
+        btnMultiply = findViewById(R.id.btnMultiply)
+        btnDivide = findViewById(R.id.btnDivide)
+
+//        Log.d("TEST", "onCreate: Activity has been created")
+//        Log.e("TEST", "onCreate: Activity has been created")
+//        Log.w("TEST", "onCreate: Activity has been created")
+//        Log.i("TEST", "onCreate: Activity has been created")
+
     }
 }
